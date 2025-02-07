d786260f2a0229a816c5ce2358ade243bca16cea
gerceboss <nilavprajapati2004@gmail.com>
2025-02-07 19:58:22 +0530

    modified shell script

---
+++ input/input_1.c
@@ -0,0 +1,7 @@
+int main() {
+    int a, b = 10;
+    a = -b--;
+    printf("a = %d, b = %d", a, b);
+    return 0;
+
+}
--- run.sh
+++ run.sh
@@ -4,18 +4,23 @@ INPUT_DIR="input"
 OUTPUT_DIR="output"
 
 rm -rf "$OUTPUT_DIR"
+rm -rf src/lexer
 mkdir -p "$OUTPUT_DIR"
 
 echo "Compiling generator.l with lex..."
-lex src/generator.l
+flex  -o src/lex.yy.cc src/generator.l
 
-echo "Compiling lex.yy.c with g++..."
-g++ -o src/a.out src/lex.yy.c -lfl
+echo "Compiling lex.yy.cc with g++..."
+g++  src/lex.yy.cc -o src/lexer -lfl
 
 echo "Running test cases from $INPUT_DIR and saving results to $OUTPUT_DIR..."
 for input_file in "$INPUT_DIR"/*; do
-    output_file="$OUTPUT_DIR/$test_name.txt"
-    src/.a.out < "$input_file" > "$output_file"
+    filename=$(basename -- "$input_file")  
+    filename_no_ext="${filename%.*}"  
+    filename_no_prefix="${filename_no_ext#input}"  
+    output_file="$OUTPUT_DIR/output${filename_no_prefix}.txt" 
+
+    src/lexer  "$input_file" > "$output_file"
 done
 
 echo "All test cases executed successfully!"
